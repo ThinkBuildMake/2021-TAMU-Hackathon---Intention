0960b39992909ac293d117abf748018c6f79a74e
Michael Chuang <michael.chuang@utexas.edu>
2021-01-31 09:43:00 -0600

    Added updates to JavaScript and Text Box

--- popup.js
+++ popup.js
@@ -1,2 +1,21 @@
 var start = document.querySelector('#start-intention');
-var startBreaj = document.querySelector('#stop-intention');
+var startBreak = document.querySelector('#stop-intention');
+
+function timer(seconds) {
+  var sec = seconds;
+  var timer = setInterval(function() {
+    sec--;
+    if (sec < 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+//When focus mode = 1 and if you switch to chrome, create an unremovable popup for 20 seconds
+if (start == 1)
+  while (1) {
+    timer(20);
+    if (startBreak == 1) {
+      break;
+    }
+  }
